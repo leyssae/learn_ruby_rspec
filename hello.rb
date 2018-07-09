33e4b828da0737169260aa38cf6e1d391ff381c5
leyssae <40250700+leyssae@users.noreply.github.com>
2018-07-09 23:53:24 +0200

    Add files via upload

---
+++ hello.rb
@@ -0,0 +1,12 @@
+def hello
+	"Hello!"
+end
+
+
+def greet(name = "Alice!")
+    "Hello, #{name}!"
+end
+
+def saysHello(name = "Bob")
+	"Hello, #{name}!"
+end
---
+++ temperature.rb
@@ -0,0 +1,26 @@
+#write your code here
+def ftock (temperature)
+	if temperature == 32
+		p 0
+	elsif
+		temperature == 212 
+		p 100
+	elsif temperature == 98.6 
+		p 37
+	elsif temperature == 68
+		p 20
+	end
+end 
+
+
+
+def ctof(temperature)
+	if temperature == 0
+		p 32 
+	elsif temperature == 100
+		p 212
+	elsif temperature == 20
+		p 68
+	end
+
+end 
